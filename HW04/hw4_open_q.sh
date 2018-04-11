5e4bda7a8e5f4a3a9260fa2644bd7e028546e2f2
Danny Langford <danny14@nrlogin1.cluster>
2018-04-10 20:19:45 -0400

    Adding newriver output

--- HW04/hw4_open_q.sh
+++ HW04/hw4_open_q.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #
 #PBS -l walltime=00:10:00
-#PBS -l nodes=1:ppn:20
+#PBS -l nodes=1:ppn=20
 #PBS -W group_list=newriver
 #PBS -q open_q
 #PBS -j oe
@@ -14,7 +14,14 @@ module load openmpi
 
 make
 
-for num in `seq 1 20`
-do
-  mpiexec -n $num main;
-done;
+./main 1
+./main 2
+./main 4
+./main 8
+./main 12
+./main 16
+./main 20
+#for num in `seq 1 20`
+#do
+  #mpiexec -n $num main;
+#done;
