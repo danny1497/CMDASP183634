97117921e8040a2bb59edd9524b8dd45faed640f
Danny <danny14@vt.edu>
2018-04-25 13:16:43 -0400

    Worked on cuda part

--- HW05/cudaDecrypt.cu
+++ HW05/cudaDecrypt.cu
@@ -7,6 +7,43 @@
 #include "cuda.h"
 #include "functions.c"
 
+__device__ int cudamodprod(unsigned int a, unsigned int b, unsigned int p) {
+  unsigned int za = a;
+  unsigned int ab = 0;
+
+  while (b > 0) {
+    if (b%2 == 1) ab = (ab +  za) % p;
+    za = (2 * za) % p;
+    b /= 2;
+  }
+  return ab;
+}
+
+//compute a^b mod p safely
+__device__ int cudamodExp(unsigned int a, unsigned int b, unsigned int p) {
+  unsigned int z = a;
+  unsigned int aExpb = 1;
+
+  while (b > 0) {
+    if (b%2 == 1) aExpb = cudamodprod(aExpb, z, p);
+    z = cudamodprod(z, z, p);
+    b /= 2;
+  }
+  return aExpb;
+}
+
+__global__ void findSecretKey(unsigned int g, unsigned int h, unsigned int p, unsigned int *dx) {
+
+  int threadID = threadIdx.x;
+  int blockID = blockIdx.x;
+  int Nblock = blockDim.x;
+  unsigned int id = threadID + (blockID*Nblock);
+  if(id<(p-1)) {
+    if (cudamodExp(g,id+1,p)==h) {
+      *dx=id+1;
+    }
+  }
+}
 
 int main (int argc, char **argv) {
 
@@ -14,10 +51,11 @@ int main (int argc, char **argv) {
      your completed decrypt.c main function. */
 
   /* Q4 Make the search for the secret key parallel on the GPU using CUDA. */
+  
   //declare storage for an ElGamal cryptosytem
   unsigned int n, p, g, h, x;
   unsigned int Nints;
-
+  int Nthreads = atoi(argv[1]);
   //get the secret key from the user
   printf("Enter the secret key (0 if unknown): "); fflush(stdout);
   char stat = scanf("%u",&x);
@@ -27,6 +65,8 @@ int main (int argc, char **argv) {
   /* Q3 Complete this function. Read in the public key data from public_key.txt
     and the cyphertexts from messages.txt. */
   //FILE *pubKey,*msg;
+  unsigned int Nblocks;
+  //unsigned int Nthreads,blocks;
   FILE *pubKey = fopen("public_key.txt","r");
   FILE *msg = fopen("message.txt","r");
   fscanf(pubKey, "%u\n%u\n%u\n%u\n",&n,&p,&g,&h);
@@ -44,24 +84,47 @@ int main (int argc, char **argv) {
 
   fclose(pubKey);
   fclose(msg);
-
+  unsigned int host = 0;
+  unsigned int *device;
+  cudaMalloc(&device,sizeof(unsigned int));
   // find the secret key
-  if (x==0 || modExp(g,x,p)!=h) {
+  /*
+  if (x==0 || cudamodExp(g,x,p)!=h) {
     printf("Finding the secret key...\n");
     double startTime = clock();
     for (unsigned int i=0;i<p-1;i++) {
-      if (modExp(g,i+1,p)==h) {
+      if (cudamodExp(g,i+1,p)==h) {
         printf("Secret key found! x = %u \n", i+1);
         x=i+1;
       } 
-    }
-    double endTime = clock();
-
-    double totalTime = (endTime-startTime)/CLOCKS_PER_SEC;
-    double work = (double) p;
-    double throughput = work/totalTime;
-
-    printf("Searching all keys took %g seconds, throughput was %g values tested per second.\n", totalTime, throughput);
+    }*/
+  if (x==0 || cudamodExp(g,x,p)!=h) {
+    printf("Finding the secret key...\n");
   }
+  double startTime = clock();   
+  //Nthreads = 32;
+  blocks = (p-1)/32;
+  findSecretKey <<<Nblocks,32>>>(g,h,p,device);
+  cudaDeviceSynchronize();
+  unsigned int size = sizeof(unsigned int);
+  cudaMemcpy(host,device,size, cudeMemcpyDeviceToHost);
+  double endTime = clock();
+
+  double totalTime = (endTime-startTime)/CLOCKS_PER_SEC;
+  double work = (double) p;
+  double throughput = work/totalTime;
+  printf("Secret key found! x %u \n", host);
+  printf("Searching all keys took %g seconds, throughput was %g values tested per second.\n", totalTime, throughput);
+  //}
+
+  //int buffer = 1024;
+  //ElGamalDecrypt(Zmessage,a,Nints,p,x);
+  unsigned int Nchars = Nints*charsPerInt;
+  unsigned char *message = (unsigned char *) malloc(buffer*sizeof(unsigned char));
+  ElGamalDecrypt(Zmessage,a,Nints,p,x);
+  //unsigned int cpi = (n-1)/8;
+  convertZToString(Zmessage,Nints,message,Nchars);
+  printf("Decrypted message: \"%s\"\n",message);
+  cudaFree(device); 
   return 0;
 }
